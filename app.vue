4a74411856195b7aa86124d00c6db7e97cbbec84
fernandolievano <fernandolievan@gmail.com>
2025-01-29 19:56:03 -0300

    implement Vuetify integration and update app structure

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <v-app>
+      <NuxtPage></NuxtPage>
+    </v-app>
+  </NuxtLayout>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <section class="px-8 py-4">
+    <h1>Daptee technical test</h1>
+  </section>
+</template>

5d0526636c629fc1810eab8222a41f22f87c554c
fernandolievano <fernandolievan@gmail.com>
2025-01-31 03:11:20 -0300

    feat: implement user management features with API integration and delete functionality

--- composables/useUsers.ts
+++ composables/useUsers.ts
@@ -1,23 +1,36 @@
+import { useAsyncData } from '#app';
 import { ref } from 'vue';
-import { fetchUsersFromAPI } from '~/utils/api';
 import type { User } from '~/interfaces/users';
 
-const users = ref<User[]>([]);
-const searchQuery = ref<String>('');
+export function useUsers() {
+  const searchQuery = ref<String>('');
+  const { data: users, status: loading, error, refresh } = useAsyncData<User[]>(
+    'users',
+    async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      if (!response.ok) throw new Error('Error al obtener usuarios');
+      return response.json();
+    },
+    {
+      server: true
+    }
+  );
 
-const fetchUsers = async () => {
-  users.value = await fetchUsersFromAPI();
-};
-const filteredUsers = computed(() => {
-  return users.value.filter(user => {
-    return user.name.toLowerCase().includes(searchQuery.value.toLowerCase());
+  const filteredUsers = computed(() => {
+    const query = searchQuery.value.toLowerCase();
+    const fields = ['name', 'email', 'username'] as const;
+
+    if (!users.value) return [];
+    return users.value.filter(user => fields.some(key => user[key].toLowerCase().includes(query)));
   });
-});
-const useUsers = () => ({
-  users,
-  searchQuery,
-  fetchUsers,
-  filteredUsers,
-});
 
-export default useUsers;
+  const deleteUser = (id: number) => {
+    if (users.value) {
+      users.value = users.value.filter(user => user.id !== id);
+    }
+  };
+
+  return {
+    users, loading, error, refresh, searchQuery, filteredUsers, deleteUser
+  };
+}
--- pages/users.vue
+++ pages/users.vue
@@ -1,5 +1,107 @@
 <template>
-  <section class="px-8 py-4">
-    <h1>Users Page</h1>
+  <section class="px-8 py-4 w-100">
+    <h1 class="mb-4">Usuarios</h1>
+
+    <v-skeleton-loader v-if="loading === 'pending'" height="400" type="list-item-two-line" class="mt-4">
+    </v-skeleton-loader>
+
+    <v-card v-if="loading === 'success' && users != null && users.length >= 1">
+      <v-list lines="two">
+        <v-list-item v-for="user in users" :key="user.id">
+          <template v-slot:prepend>
+            <v-avatar icon="mdi-account">
+            </v-avatar>
+          </template>
+          <v-list-item-title>{{ user.name }}</v-list-item-title>
+          <v-list-item-subtitle>{{ user.email }}</v-list-item-subtitle>
+
+          <!-- options button -->
+          <template v-slot:append>
+            <v-menu>
+              <template v-slot:activator="{ props }">
+                <v-btn icon="mdi-dots-vertical" variant="text" v-bind="props"></v-btn>
+              </template>
+
+              <!-- options list -->
+              <v-list>
+                <v-list-item @click="showUser(user)">Ver usuario</v-list-item>
+                <v-dialog v-model="confirmDelete" persistent max-width="800">
+                  <template v-slot:activator="{ props: activatorProps }">
+                    <v-list-item v-bind="activatorProps">
+                      Eliminar usuario
+                    </v-list-item>
+                  </template>
+
+                  <v-card>
+                    <v-card-text>
+                      <p class="text-center">
+                        ¿Realmente deseas eliminar al usuario <b>{{ user.name }}</b> de la lista?
+                      </p>
+                    </v-card-text>
+                    <template v-slot:actions>
+                      <v-spacer></v-spacer>
+
+                      <v-btn @click="confirmDelete = false">
+                        Cancelar
+                      </v-btn>
+
+                      <v-btn color="red" @click="deleteUser(user.id)">
+                        Eliminar
+                      </v-btn>
+                    </template>
+                  </v-card>
+                </v-dialog>
+              </v-list>
+              <!-- options list -->
+            </v-menu>
+          </template>
+          <!-- options button -->
+        </v-list-item>
+      </v-list>
+    </v-card>
+
+    <!-- user modal -->
+    <v-dialog v-model="userModal" max-width="400">
+      <v-card v-if="selectedUser">
+        <v-card-title class="text-center pt-8 pb-4">
+          <v-icon size="x-large">mdi-account</v-icon>
+          <h2>{{ selectedUser.username }}</h2>
+        </v-card-title>
+        <v-card-text>
+          <p><strong>Nombre:</strong> {{ selectedUser.name }}</p>
+          <p><strong>Email:</strong> {{ selectedUser.email }}</p>
+          <p><strong>Teléfono:</strong> {{ selectedUser.phone }}</p>
+          <p><strong>Compañía:</strong> {{ selectedUser.company.name }}</p>
+          <p><strong>Website</strong>:
+            <a :href="`https://${selectedUser.website}`" target="_blank" rel="noopener noreferrer">
+              {{ selectedUser.website }}
+            </a>
+          </p>
+          <p><strong>Dirección:</strong> {{ selectedUser.address.suite }}, {{ selectedUser.address.street }}, {{
+            selectedUser.address.city }}</p>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="secondary" block @click="userModal = false">Cerrar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <!-- user modal -->
   </section>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { useUsers } from '~/composables/useUsers';
+import type { User } from '~/interfaces/users';
+
+const confirmDelete = ref(false);
+const userModal = ref(false);
+const selectedUser = ref<User | null>(null);
+
+const { users, loading, error, searchQuery, filteredUsers, deleteUser } = useUsers();
+
+const showUser = (user: User) => {
+  selectedUser.value = user;
+  userModal.value = true;
+};
+</script>
